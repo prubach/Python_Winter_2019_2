21dc3e04b360189b550f6bff8f973d42b6691466
prubac <pawel.rubach@sgh.waw.pl>
2020-01-16 14:43:37 +0100

    DownloadWorker - multithreaded

--- knotprot_multithreading.py
+++ knotprot_multithreading.py
@@ -1,4 +1,6 @@
 from knotprot_download import get_proteins, setup_download_dir, download_link, time_it
+from threading import Thread
+from queue import Queue
 
 def run_single_thread(dir):
     proteins = get_proteins()
@@ -7,5 +9,35 @@ def run_single_thread(dir):
     for p in proteins:
         download_link(dir, p)
 
+### Download using separate threads ###
+class DownloadWorker(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            dir, prot = self.queue.get()
+            try:
+                download_link(dir, prot)
+            finally:
+                self.queue.task_done()
+
+def run_workers(dir):
+    proteins = get_proteins()
+    #print(len(proteins))
+    #print(proteins)
+    queue = Queue()
+    for n in range(30):
+        worker = DownloadWorker(queue)
+        # Don't wait until this thread exits, stop it when main thread exits
+        worker.daemon = True
+        worker.start()
+    for p in proteins:
+        queue.put((dir, p))
+    queue.join()
+
+
 dir = setup_download_dir()
-time_it(run_single_thread, dir)
+#time_it(run_single_thread, dir)
+time_it(run_workers, dir)
